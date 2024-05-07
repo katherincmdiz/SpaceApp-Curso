17c4e5236cc3eefcf053cdf132f8b1e8dc1a259c
kdcm08 <156930985+katherincmdiz@users.noreply.github.com>
2024-05-06 23:27:47 -0300

    Add files via upload

---
+++ ejemplos/sincrono.js
@@ -0,0 +1,36 @@
+function cuatro () {
+    console.log("cuatro")
+}
+
+function tres () {
+    console.log("tres")
+    cuatro()
+}
+
+function dos () {
+    console.log("dos")
+    tres()
+}
+
+function uno () {
+    console.log("uno")
+    dos()
+}
+
+uno()
+
+//<ul class="ul__cards" data-ul><li class="card">
+<img class="card__image" src="assets/BannerSpaceApp.png" alt="imagen">
+<h3 class="card__title">Educación y Tecnologia</h3>
+</li>
+<li class="card">
+<img class="card__image" src="assets/BannerSpaceApp.png" alt="imagen">
+<h3 class="card__title">Educación y Tecnologia</h3>
+</li>
+<li class="card">
+<img class="card__image" src="assets/BannerSpaceApp.png" alt="imagen">
+<h3 class="card__title">Educación y Tecnologia</h3>
+</li>
+<li class="card">
+<img class="card__image" src="assets/BannerSpaceApp.png" alt="imagen">
+<h3 class="card__title">Educación y Tecnologia</h3>
